e06c2b49240d565987483e7b5707804fa49b70a3
Linus Heckemann <git@sphalerite.org>
2022-02-22 00:37:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,83 @@
+# Simple Nix-based OS for Unifi AP (AC-Lite)
+
+Playing around with nixpkgs's cross-compilation support.
+
+## Status
+
+- Linux boots!
+- Just an initramfs for the filesystem.
+- Ethernet works in Linux.
+- WiFi probably should work, but I haven't managed to configure hostapd right yet.
+
+## How to use
+
+You need:
+- A Unifi AP AC-Lite and its PoE injector
+- An Ethernet port available on your workstation
+- Two Ethernet cables
+- A USB-UART adapter and some wires to connect it
+
+(you may want to skip ahead and start the build before proceeding, as the first build may take a while).
+
+Crack open the AP's case. This model isn't held together by screws,
+only plastic snap-in latch thingies. You should see two sets of holes
+which look like they could be serial ports. You'll want the one close
+to the metal can.
+
+Connect the serial port to your adapter:
+
+- device ground to adapter ground (black in my picture)
+- device TX to adapter RX (green in my picture)
+- device RX to adapter TX (blue in my picture)
+
+![Serial console](serial.jpg)
+Sorry for the potato image quality. The important part should be visible though.
+
+Also connect an Ethernet cable from your workstation to the LAN port
+on the PoE injector, and another from the injector's POE port to the
+device. Don't connect the injector to mains power yet!
+
+Configure a static IPv4 address on the interface, e.g. on NixOS:
+
+```nix
+{
+  networking.interfaces.enp5s0.ipv4.addresses = [{ address = "192.168.99.1"; prefixLength = 24; }];
+}
+```
+
+Then prepare the boot images, open the firewall, and start the TFTP server:
+
+```
+mkdir -p tftp
+nix-build -A boot -o tftp/boot
+sudo iptables -I INPUT 1 -d 192.168.99.1 -p udp --dport 69 -j ACCEPT
+sudo nix-shell -p atftp --run 'atftpd --daemon --logfile - --no-fork ./tftp'
+```
+
+Open up your serial console, for example with `picocom -b 115200 /dev/ttyUSB0`.
+
+Then power on your UAP-AC-LITE, interrupt the boot timeout, and perform some initial setup when the `ath>` prompt appears.
+
+⚠️ This will prevent booting the original firmware automatically! ⚠️
+```
+ath> set bootcmd_ubnt $bootcmd
+ath> set ipaddr 192.168.99.2
+ath> set serverip 192.168.99.1
+ath> set fetchcmd tftpboot 0x84000000 kernel.img && tftpboot 0x84ffffc0 initramfs.img
+ath> set bootcmd run fetchcmd && bootm 0x84000000 0x84ffffc0
+ath> set bootdelay 5
+ath> saveenv
+ath> boot
+```
+
+From then on, the AP should try to fetch and boot kernel and initramfs
+from your workstation instead of booting the built-in firmware. This
+allows for a fairly fast development cycle.
+
+## Todos and ideas for the future
+
+- Get WiFi working
+- Add dropbear to the image
+- Compare and contrast with [nixwrt](https://github.com/telent/nixwrt) to see if this is worth adapting to that structure
+- Same thing but for [celun](https://github.com/celun/celun)
+- Flashable images?
---
+++ default.nix
@@ -0,0 +1,217 @@
+{ nixpkgs ? builtins.fetchTarball {
+    url = "https://github.com/nixos/nixpkgs/archive/58dae9ca1c2c52990e45f358b680e8411a9dfab1.tar.gz";
+  }
+, system ? builtins.currentSystem
+}:
+import nixpkgs {
+  inherit system;
+  crossSystem = {
+    libc = "musl";
+    config = "mips-unknown-linux-musl";
+    openssl.system = "linux-generic32";
+    withTLS = true;
+
+    name = "ath79"; # idk
+    linux-kernel = {
+      name = "ath79";
+      target = "uImage";
+      installTarget = "uImage";
+      autoModules = false;
+    };
+  };
+  config.allowUnsupportedSystem = true;
+  overlays = [(self: super: let inherit (self) lib; in {
+    openwrt-src = builtins.fetchGit {
+      url = https://git.openwrt.org/openwrt/openwrt.git;
+      rev = "cbfce9236754700a343632fff8e035acbc1b1384";
+    };
+
+    initramfs = super.makeInitrd {
+      compressor = "${self.pkgsBuildHost.zstd}/bin/zstd";
+      makeUInitrd = true;
+      contents = [{
+        object = "${self.busybox}/bin";
+        symlink = "/bin";
+      } {
+        object = super.writeScript "r" ''
+          #!/bin/sh
+          echo b > /proc/sysrq-trigger
+        '';
+        symlink = "/sbin/r";
+      } {
+        object = super.writeScript "debug-wifi" ''
+          #!/bin/sh
+          echo 0xffffffff > /sys/module/ath10k_core/parameters/debug_mask
+          echo 8 > /proc/sys/kernel/printk
+          cd /sys/bus/pci/drivers/ath10k_pci
+          echo 0000:00:00.0 > unbind
+          echo 0000:00:00.0 > bind
+        '';
+        symlink = "/sbin/debug-wifi";
+      } {
+        object = super.writeScript "init" ''
+          #!/bin/sh
+          set -x
+          mount -t devtmpfs none /dev
+          mount -t proc proc /proc
+          mount -t sysfs sys /sys
+          mkdir -p /run
+          mount -t tmpfs tmpfs /run
+          ${self.eudev}/bin/udevd &
+          ip l set eth0 up
+          ${self.cal-wifi}
+          exec sh
+        '';
+        symlink = "/init";
+      } {
+        object = super.runCommandNoCC "firmware-ath10k" {} ''
+          mkdir -p $out/ath10k
+          cp -r ${self.firmwareLinuxNonfree}/lib/firmware/ath10k/QCA988X $out/ath10k/
+          cp -r ${self.wireless-regdb}/lib/firmware/regulatory.db $out/
+        '';
+        symlink = "/lib/firmware";
+      } {
+        object = self.hostapd;
+        symlink = "/hostapd";
+      } {
+        object = super.writeText "hostapd.conf" ''
+          interface=wlan0
+          ssid=test
+          country_code=DE
+          wpa=1
+          wpa_psk=d27c89adbd8dd3f811a09bb662e78441a4842517486af5b9a4b377f460fd9fc7
+          wpa_pairwise=CCMP
+          hw_mode=a
+
+          wmm_enabled=1
+          ieee80211n=1
+          ieee80211ac=1
+
+          channel=0
+        '';
+        symlink = "/etc/hostapd.conf";
+      } ];
+    };
+
+    cal-wifi = super.writeScript "cal-wifi" ''
+      #!/bin/sh
+      mtd=$(grep '"art"' /proc/mtd | cut -d : -f 1)
+      dd if=/dev/$mtd of=/lib/firmware/ath10k/cal-pci-0000:00:00.0.bin iflag=skip_bytes,fullblock bs=$((0x844)) skip=$((0x5000)) count=1
+    '';
+
+    lib = super.lib // {
+      elementsInDir = dir: lib.mapAttrsToList (name: type: { inherit type name; path = dir + "/${name}"; }) (builtins.readDir dir);
+      filesInDir = dir: map ({ path, ... }: path) (super.lib.filter (entry: entry.type == "regular") (lib.elementsInDir dir));
+    };
+
+    kernelSrc = (super.applyPatches {
+      inherit (self.linux_5_10) src;
+      patches = []
+      ++ (lib.filesInDir "${self.openwrt-src}/target/linux/generic/backport-5.10")
+      ++ (lib.filesInDir "${self.openwrt-src}/target/linux/generic/pending-5.10")
+      ++ (lib.filesInDir "${self.openwrt-src}/target/linux/ath79/patches-5.10")
+      ;
+    }).overrideAttrs (o: {
+      prePatch = ''
+        (
+        ${self.pkgsBuildHost.rsync}/bin/rsync -rt ${self.openwrt-src}/target/linux/generic/files/ ./
+        ${self.pkgsBuildHost.rsync}/bin/rsync -rt ${self.openwrt-src}/target/linux/ath79/files/ ./
+        )
+      '';
+    });
+
+    kernel = (super.buildLinux {
+      inherit (super.linux_5_10) version;
+      src = self.kernelSrc;
+      defconfig = "ath79_defconfig";
+      useCommonConfig = false;
+      autoModules = false;
+      ignoreConfigErrors = false;
+      structuredExtraConfig = with super.lib.kernel; super.lib.mkForce {
+        MAGIC_SYSRQ = yes;
+        MIPS_RAW_APPENDED_DTB = yes;
+        DEVTMPFS = yes;
+        TMPFS = yes;
+
+        # Debugging
+        IKCONFIG = yes;
+        IKCONFIG_PROC = yes;
+
+        SPI_AR934X = yes;
+
+        # Ethernet
+        AG71XX = yes;
+        #GENERIC_PHY = yes;
+        #GENERIC_PINCONF = yes;
+        PINCTRL_SINGLE = yes;
+        AT803X_PHY = yes;
+        REGULATOR = yes;
+
+        #MDIO_GPIO = yes;
+        #MDIO_I2C = yes;
+        MFD_SYSCON = yes;
+
+        # WiFi
+        PCI = yes;
+        PCI_AR724X = yes;
+        CFG80211 = yes;
+        MAC80211 = yes;
+        ATH_COMMON = yes;
+        ATH10K = yes;
+        ATH10K_PCI = yes;
+        ATH10K_DEBUG = yes;
+
+        # Other
+        IPV6 = yes;
+
+        # minimalisation
+        ATH9K = no;
+        RTW88 = no;
+        MODULES = yes;
+      };
+      kernelPatches = [];
+    }).overrideAttrs (o: rec {
+      installPhase = ''
+        mkdir -p $out
+        cp -v arch/mips/boot/uImage $out/
+        cp -v arch/mips/boot/vmlinu[xz]* $out/
+        cp -v vmlinux $out/
+      '';
+    });
+
+    dtb = super.runCommandCC "uaclite.dtb" { nativeBuildInputs = [ self.pkgsBuildHost.dtc ]; } ''
+      unpackFile ${self.kernel.src}
+      $CC -E -nostdinc -x assembler-with-cpp -I linux*/include ${self.openwrt-src}/target/linux/ath79/dts/qca9563_ubnt_unifiac-lite.dts -o - | dtc -o $out
+    '';
+
+    boot = super.runCommandCC "boot" {
+      nativeBuildInputs = [
+        self.pkgsBuildHost.ubootTools
+        self.pkgsBuildHost.pigz
+      ];
+    } ''
+      set -x
+      PS4=' $ '
+      mkdir -p $out
+      cd $out
+
+      cat ${self.kernel}/vmlinux.bin ${self.dtb} > vmlinux.bin
+      pigz -9 vmlinux.bin
+
+      mkimage \
+        -A mips \
+        -O linux \
+        -C gzip \
+        -T kernel \
+        -a 0x80060000 \
+        -n Linux-${self.kernel.version} \
+        -d vmlinux.bin.gz \
+        $out/kernel.img
+
+      ln -s ${self.initramfs}/initrd.img initramfs.img
+
+      ls -lh $(readlink -f initramfs.img kernel.img)
+      set +x
+    '';
+  })];
+}
